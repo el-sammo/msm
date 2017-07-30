ccb0fe6aba1d80c7d1719cf079a68f8974ec2af7
Jon Barrett <jonab1974@gmail.com>
2017-07-30 21:16:38 +0000

    wiring up ng-clicks

--- assets/controllers/LayoutController.js
+++ assets/controllers/LayoutController.js
@@ -13,14 +13,14 @@
 	controller.$inject = [
 		'navMgr', 'pod', '$scope', '$window',
 		'$http', '$routeParams', '$modal', 'layoutMgmt',
-		'$rootScope', 'playerMgmt',
+		'$rootScope', 'customerMgmt',
 		'signupPrompter', 'deviceMgr'
 	];
 
 	function controller(
 		navMgr, pod, $scope, $window,
 		$http, $routeParams, $modal, layoutMgmt,
-		$rootScope, playerMgmt,
+		$rootScope, customerMgmt,
 		signupPrompter, deviceMgr
 	) {
 
@@ -75,25 +75,12 @@
 			signupPrompter.welcome();
 		}
 
-		var bgImgs = [
-			'baseball_wide',
-			'basketball_wide',
-			'football_wide',
-			'hockey_wide',
-			'racing_wide',
-			'soccer_wide'
-		];
-
-		var randImg = bgImgs[Math.floor(Math.random() * bgImgs.length)];
-
-		$scope.bgImg = randImg;
-
-		var sessionPromise = playerMgmt.getSession();
+		var sessionPromise = customerMgmt.getSession();
 		sessionPromise.then(function(sessionData) {
-			if(sessionData.playerId) {
+			if(sessionData.customerId) {
 				$scope.showLogout = true;
 				$scope.accessAccount = true;
-				$scope.playerId = sessionData.playerId;
+				$scope.customerId = sessionData.customerId;
 			}
 
 			$scope.showAccount = function() {
@@ -111,11 +98,11 @@
 			return string.charAt(0).toUpperCase() + string.slice(1);
 		}
 
-		$rootScope.$on('playerLoggedIn', function(evt, args) {
+		$rootScope.$on('customerLoggedIn', function(evt, args) {
 			$scope.showLogout = true;
 			$scope.accessAccount = true;
-			$scope.playerId = args;
-			$rootScope.$broadcast('orderChanged');
+			$scope.customerId = args;
+			$rootScope.$broadcast('customerLoggedIn');
 		});
 	}
 
